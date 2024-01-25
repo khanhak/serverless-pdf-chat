2e5df1a90ad15baa8d5937132335ebd772d90e02
khanhak <76500364+khanhak@users.noreply.github.com>
2024-01-24 22:42:10 -0500

    separate DynamoDB in GenResponse

--- backend/src/generate_response/main.py
+++ backend/src/generate_response/main.py
@@ -11,6 +11,7 @@ from langchain.chains import ConversationalRetrievalChain
 
 
 MEMORY_TABLE = os.environ["MEMORY_TABLE"]
+MEMORY_TABLE2 = os.environ["MEMORY_TABLE2"]
 BUCKET = os.environ["BUCKET"]
 
 
@@ -48,10 +49,20 @@ def lambda_handler(event, context):
     message_history = DynamoDBChatMessageHistory(
         table_name=MEMORY_TABLE, session_id=conversation_id
     )
+    message_history2 = DynamoDBChatMessageHistory(
+        table_name=MEMORY_TABLE2, session_id=conversation_id
+    )
 
     memory = ConversationBufferMemory(
         memory_key="chat_history",
         chat_memory=message_history,
+        input_key="question",
+        output_key="answer",
+        return_messages=True,
+    )
+    memory2 = ConversationBufferMemory(
+        memory_key="chat_history",
+        chat_memory=message_history2,
         input_key="question",
         output_key="answer",
         return_messages=True,
@@ -63,15 +74,16 @@ def lambda_handler(event, context):
         memory=memory,
         return_source_documents=True,
     )
+    qa2 = ConversationalRetrievalChain.from_llm(
+        llm=llm,
+        retriever=faiss_index.as_retriever(),
+        memory=memory2,
+        return_source_documents=True,
+    )
 
     res = qa({"question": human_input})
-    ans = '''" Based on the context provided, it seems you are asking for more clarification on Form 1098-T and the information it contains. \n\nForm 1098-T is a tuition statement that eligible educational institutions send to students to report payments received for qualified tuition and related expenses in a calendar year. It contains information like:\n\n- The total payments received by the school from any source for the student's qualified tuition and related expenses in the calendar year (Box 1)\n\n- Any reimbursements or refunds made for qualified tuition and related expenses during the year that relate to payments received in the same calendar year (Box 1) \n\n- Any prior year adjustments made by the school for qualified tuition and related expenses reported on a prior year's Form 1098-T (Box 4)\n\n- Total scholarships or grants administered and processed by the school during the calendar year (Box 5)\n\n- Any prior year adjustments made to scholarships or grants reported on a prior year's Form 1098-T (Box 6)\n\nThe student uses this form when completing their tax return to determine eligibility for education tax credits like the American Opportunity Credit or Lifetime Learning Credit. \n\nPlease let me know if you need any clarification on a specific aspect of the",
-        "source_documents": [
-          "page_content='Box 10. Shows the total amount of reimbursements or refunds of qualified tuition and \\nrelated expenses made by an insurer. The amount of reimbursements or refunds for the \\ncalendar year may reduce the amount of any education credit you can claim for the year \\n(may result in an increase in tax liability for the year of the refund). \\nFuture developments.  For the latest information about developments related to Form \\n1098-T and its instructions, such as legislation enacted after they were published, go to \\nwww.irs.gov/Form1098T . \\nFree File Program.  Go to www.irs.gov/FreeFile to see if you qualify for no-cost online \\nfederal tax preparation, e-filing, and direct deposit or payment options.\\nThe information shown on this form has been provided to the IRS.\\nDetailed financial information is available to the student at Wolverine Access>Student Business\\nhttps://wolverineaccess.umich.edu\\nThe University of Michigan cannot provide individual tax advice and shall not be liable for damages of any' metadata={'source': '/tmp/HANEEF-KHAN-1098T.pdf', 'page': 1}",
-          "page_content='Box 2. Reserved for future use. \\nBox 3. Reserved for future use. \\nBox 4. Shows any adjustment made by an eligible educational institution for a prior \\nyear for qualified tuition and related expenses that were reported on a prior year \\nForm 1098-T. This amount may reduce any allowable education creditthat you claimed for the prior year (may result in an increase in tax liability for the year of the\\nrefund). See \"recapture\" in the index to Pub. 970 to report a reduction in your education \\ncredit or tuition and fees deduction. \\nBox 5. Shows the total of all scholarships or grants administered and processed by the \\neligible educational institution. The amount of scholarships or grants for the calendar year \\n(including those not reported by the institution) may reduce the amount of the education \\ncredit you claim for the year. TIP: You may be able to increase the combined value of an \\neducation credit and certain educational assistance (including Pell Grants) if the student' metadata={'source': '/tmp/HANEEF-KHAN-1098T.pdf', 'page': 1}",
-          "page_content=\"£CORRECTED\\nFILER'S name, street address, city or town, province or state, country, ZIP or \\nforeign postal code, and telephone number\\nThe University of Michigan\\n2226 Student Activities Bldg\\n515 East Jefferson St\\nAnn Arbor, MI 48109-1316\\nStudent Financial Services: 877/840-47381 Payments received for \\nqualified tuition and related \\nexpenses\\n$ 31223.19OMB No.1545-1574\\n2023\\nForm 1098- TTuition\\nStatement 2  \\nFILER'S federal Identification no.\\n386006309STUDENT'S  taxpayer identification no.\\n*****26673  Copy B\\nFor Student\\n       This is \\nimportant \\n         tax information\\nand is being \\nfurnished to the \\nInternal Revenue \\nService. This form \\nmust be used to \\ncomplete Form 8863\\nto claim education \\ncredits. Give it to the\\ntax preparer or use it\\nto prepare the tax \\nreturn.  STUDENT'S name\\nHaneef  A  Khan  4  Adjustments made for a \\nprior year\\n$ 5  Scholarships or       \\ngrants\\n$ \\nStreet address (including apt. no.)\\n43122 Meadow Grove Dr6  Adjustments to  \\nscholarships or grants for \\na prior year\" metadata={'source': '/tmp/HANEEF-KHAN-1098T.pdf', 'page': 0}",
-          "page_content=\"provider. Although the filer or the service provider may be able to answer certain \\nquestions about the statement, do not contact the filer or the service provider for \\nexplanations of the requirements for (and how to figure) any education credit that you\\nmay claim. \\nStudent's taxpayer identification number (TIN) . For your protection, this form may \\nshow only the last four digits of your TIN (SSN, ITIN, ATIN, or EIN). However, the \\nissuer has reported your complete TIN to the IRS. Caution: If your TIN is not shown \\nin this box, your school was not able to provide it. Contact your school if you have \\nquestions. \\nAccount number . May show an account or other unique number the filer assigned to\\ndistinguish your account. \\nBox 1. Shows the total payments received by an eligible educational institution in \\n2023 from any source for qualified tuition and related expenses less any \\nreimbursements or refunds made during 2023 that relate to those payments received \\nduring 2023.\" metadata={'source': '/tmp/HANEEF-KHAN-1098T.pdf', 'page': 1}"'''
-    logger.info(res)
+    res2 = qa2({"question": human_input})
+    logger.info({"res": [res, res2]})
 
     return {
         "statusCode": 200,
@@ -81,6 +93,5 @@ def lambda_handler(event, context):
             "Access-Control-Allow-Origin": "*",
             "Access-Control-Allow-Methods": "*",
         },
-        # "body": json.dumps(res["answer"]),
-        "body": json.dumps(ans)
+        "body": json.dumps(res["answer"]),
     }
--- deploy.sh
+++ deploy.sh
@@ -1,22 +1,38 @@
 #!/usr/bin/bash
-cd /backend/
+cd backend
 # Your AWS SAM stack name
-STACK_NAME="serverless-pdf-chat"
+STACK_NAME="serverless-rizzpiss"
+
+#Create samconfig.toml
+echo "version = 0.1" > samconfig.toml
+echo "[default.deploy.parameters]" >> samconfig.toml
+echo "stack_name = \"$STACK_NAME\"" >> samconfig.toml
+echo "resolve_s3 = true" >> samconfig.toml
+echo "s3_prefix = \"$STACK_NAME\"" >> samconfig.toml
+echo "region = \"us-east-1\"" >> samconfig.toml
+echo "confirm_changeset = true" >> samconfig.toml
+echo "capabilities = \"CAPABILITY_IAM\"" >> samconfig.toml
+echo "disable_rollback = false" >> samconfig.toml
+echo "parameter_overrides = \"Frontend=\\\"local\\\" Repository=\\\"\\\"\"" >> samconfig.toml
+echo "image_repositories = []" >> samconfig.toml
+
 
 #create bucket
-random_string=$(cat /dev/urandom | tr -dc 'a-zA-Z0-9' | fold -w 8 | head -n 1)
-s3_bucket_name="serverless-pdf-chat-bucket${random_string}"
+# random_string=$(cat /dev/urandom | tr -dc 'a-zA-Z0-9' | fold -w 8 | head -n 1)
+# s3_bucket_name="serverless-pdf-chat-bucket${random_string}"
 
-# aws s3 mb s3://${s3_bucket_name}
-# echo "s3_bucket = ${s3_bucket_name}" >> .samconfig.toml
-
+sam build --parallel
 # Deploy SAM application
-sam deploy --config-file samconfig.toml --stack-name "serverless-pdf-chat" --template-file template.yaml --region "us-east-1" --no-confirm-changeset
+sam deploy --stack-name $STACK_NAME --region "us-east-1" --no-confirm-changeset
 
 # # Fetch Cognito information
 api_endpoint=$(aws cloudformation describe-stacks --stack-name $STACK_NAME --query "Stacks[0].Outputs[?OutputKey=='ApiGatewayBaseUrl'].OutputValue" --output text)
 user_pool_id=$(aws cloudformation describe-stacks --stack-name $STACK_NAME --query "Stacks[0].Outputs[?OutputKey=='CognitoUserPool'].OutputValue" --output text)
 user_pool_client_id=$(aws cloudformation describe-stacks --stack-name $STACK_NAME --query "Stacks[0].Outputs[?OutputKey=='CognitoUserPoolClient'].OutputValue" --output text)
+
+#create & confirm sample cognito user
+aws cognito-idp sign-up   --region "us-east-1"   --client-id $user_pool_client_id   --username a@a.com   --password qwertyuiop
+aws cognito-idp admin-confirm-sign-up   --region "us-east-1"   --user-pool-id $user_pool_id   --username a@a.com
 
 # # Create .env.development file in root/frontend
 echo "VITE_REGION=us-east-1" > ../frontend/.env.development
